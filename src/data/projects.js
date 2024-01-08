34c2cba3336284ff780c7e53b43ea59c7667b7d5
ScheWann <92758590+ScheWann@users.noreply.github.com>
2024-01-08 14:01:40 -0600

    new update

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,7 +1,25 @@
 // TODO Add a couple lines about each project
 const data = [
   {
-    title: "Design and implementation of integrated game website ",
+    title: "Electrical activity visualization analysis tool for diagnosing epileptic seizures and propagation",
+    subtitle: "Electrical activity visualization analysis tool for diagnosing epileptic seizures and propagation",
+    image: "/images/projects/epliepsy.png",
+    link: "https://schewann.github.io/CS529_VDS_Epilepsy/",
+    date: "2023-08",
+    desc:
+      "To enhance the diagnosis and management of patients with severe epilepsy, it's imperative to detect epileptic seizures using EEG signals from multiple electrodes accurately. This project endeavors to introduce a sophisticated visual analysis tool. This will empower physicians to more accurately determine the origin and the intracranial propagation pathways of epileptic activity."
+  },
+  {
+    title: "Gun death distribution",
+    subtitle: "Gun death distribution",
+    image: "/images/projects/cs529.png",
+    link: "https://github.com/ScheWann/CS529HW1",
+    date: "2023-08",
+    desc:
+      "In this visualization, the map is bifurcated into two distinct sections to represent states and counties, offering a nuanced view of gun death distribution across these geographical divisions. It incorporates a packed bubble chart to articulate extensive data from large U.S. counties and a stacked bar chart that provides in-depth information for each state. Adding to its interactivity and detailed exploration, an interactive scatter plot reveals statistics about male and female victims of gun deaths when a state on the map is selected, thereby enhancing the overall clarity and effectiveness of the visualization."
+  },
+  {
+    title: "Design and implementation of integrated game website",
     subtitle: "game website",
     image: "/images/projects/game.png",
     link: "https://github.com/ScheWann/gamewebsite",
